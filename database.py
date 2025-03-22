d1890ff82f47d39646b3a74532ff28b10c5fb04e
Kshiti.Mohan <kshiti.mohan@airlinq.com>
2025-03-22 15:55:11 +0530

    added docker image for mysql instead of localhost

--- database.py
+++ database.py
@@ -1,17 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-# MySQL Connection Details
-MYSQL_USER = "bssuser"
-MYSQL_PASSWORD = "bssuser@123"
-MYSQL_HOST = "localhost"
-MYSQL_PORT = "3306"
-MYSQL_DB = "employee_db"
+# Environment Variables for MySQL (Defined in docker-compose.yml)
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")  # Docker Compose service name
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_DB = os.getenv("MYSQL_DB")
 
-DATABASE_URL = "mysql+pymysql://bssuser:Bssuser%40123@localhost:3306/employee_db"
-
-# DATABASE_URL = r"mysql+pymysql://bssuser:Bssuser@123@localhost:3306/employee_db" r used for raw format of the string
-# r before a string makes it a raw string, meaning backslashes (\) are treated as literal characters instead of escape sequences.
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
 # Create Engine
 engine = create_engine(DATABASE_URL)
